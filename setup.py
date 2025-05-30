197419aceae035725720ef53eee8618531ea48a2
Redazn <146597521+Redazn@users.noreply.github.com>
2025-05-31 00:53:44 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cognitron",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Framework general-purpose untuk Cognitive meaning berdasarkan Cognitron",
+    author="MuhammadDimasPrabowo",
+    url="https://github.com/Redazn/Cognitron",
+)
